f88e3c23f351826b75218bae94b3394b5f44e7bf
omarhatemsobhymostafa <omar.hatem.okasha@gmail.com>
2026-05-01 20:48:08 +0300

    BOM error

--- src/Components/Haml/FirstPartHaml.jsx
+++ src/Components/Haml/FirstPartHaml.jsx
@@ -1,4 +1,4 @@
-﻿import React from "react";
+﻿import react from 'reacm';
 import { Link } from "react-router-dom";
 import weeksData from "../../weeks.jsx";
 
--- src/Components/Haml/WeekHamlPart1.jsx
+++ src/Components/Haml/WeekHamlPart1.jsx
@@ -1,4 +1,4 @@
-﻿import React from "react";
+﻿import react from 'react'
 import { useParams } from "react-router-dom";
 import FirstPartHaml from "./FirstPartHaml";
 import weeksData from "../../weeks.jsx";
